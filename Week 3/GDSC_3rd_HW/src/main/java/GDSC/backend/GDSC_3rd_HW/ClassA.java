9d94966e1f9daab681f4b570e48970dcae7e3111
JJH728 <72532462+JJH728@users.noreply.github.com>
2023-10-03 11:01:38 +0900

    Week3 과제 제출

---
+++ Week 3/GDSC_3rd_HW/src/main/java/GDSC/backend/GDSC_3rd_HW/ClassA.java
@@ -0,0 +1,6 @@
+package GDSC.backend.GDSC_3rd_HW;
+
+import org.springframework.stereotype.Component;
+@Component
+public class ClassA {
+}
---
+++ Week 3/GDSC_3rd_HW/src/main/java/GDSC/backend/GDSC_3rd_HW/ClassB.java
@@ -0,0 +1,11 @@
+package GDSC.backend.GDSC_3rd_HW;
+
+import org.springframework.stereotype.Component;
+@Component
+public class ClassB {
+    private ClassA classA;
+
+    public ClassB(ClassA classA){
+        this.classA=classA;
+    }
+}
